c98a9207d9408b8099625ba36315677f04fbf595
Jack-Pettigrew <jak.pettigrew@gmail.com>
2020-12-29 17:57:14 +0000

    Created TimeProgressor and TimeProgressDirector

---
+++ Assets/Scripts/Time Progressor/TimeProgressDirector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace DD.TimeProgression
+{
+    public class TimeProgressDirector : MonoBehaviour
+    {
+        // Director
+        private PlayableDirector director;
+        [SerializeField] private PlayableAsset introTimeline;
+        [SerializeField] private PlayableAsset timeProgressTimeline;
+
+        // Callback
+        private Action onCompleteCallback = null;
+
+        private void Awake()
+        {
+            director = GetComponent<PlayableDirector>();
+            director.playOnAwake = false;
+
+            onCompleteCallback = null;
+        }
+
+        public void PlayDirector(Action onCompleteCallback = null)
+        {
+            if (onCompleteCallback != null)
+                this.onCompleteCallback = onCompleteCallback;
+
+            director.stopped += PlayProgressionTimeline;
+            director.Play(introTimeline);
+        }
+
+        private void PlayProgressionTimeline(PlayableDirector playableDirector)
+        {
+            playableDirector.stopped -= PlayProgressionTimeline;
+
+            director.stopped += InvokeCompleteCallback;
+            director.Play(timeProgressTimeline);
+        }
+
+        private void InvokeCompleteCallback(PlayableDirector playableDirector)
+        {
+            playableDirector.stopped -= InvokeCompleteCallback;
+
+            onCompleteCallback?.Invoke();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Time Progressor/TimeProgressor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace DD.TimeProgression
+{
+    public class TimeProgressor : MonoBehaviour
+    {
+        [SerializeField]
+        private TimeProgressDirector director;
+
+        // Coroutine
+        private Coroutine completionCoroutine;
+
+        private void Awake()
+        {
+            if(!director)
+            {
+                Debug.LogError($"PlayableDirector has no reference on: {gameObject.name}");
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            StartProgression();
+        }
+
+        private void StartProgression()
+        {
+            // Tell SceneLoader to Async next scene
+
+            // Play while loading Async
+            director.PlayDirector();
+        }
+
+        private void CompleteProgression()
+        {
+            completionCoroutine = StartCoroutine(CompletionCoroutine());
+        }
+
+        private IEnumerator CompletionCoroutine()
+        {
+            // Wait until SceneLoader has loaded the next scene
+
+            // Fade out
+            
+            // Reset everything/Remove this scene (dependant on project requirements)
+
+            completionCoroutine = null;
+            yield return null;
+        }
+    } 
+}
