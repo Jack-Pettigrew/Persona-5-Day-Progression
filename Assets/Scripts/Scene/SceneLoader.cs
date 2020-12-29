40edccdec097cda1bf1831193854f2b537780a39
Jack-Pettigrew <jak.pettigrew@gmail.com>
2020-12-29 17:57:27 +0000

    Created base SceneLoader scripts

---
+++ Assets/Scripts/Scene/SceneLoader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DD.Scene
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public bool IsSceneLoaded { private set; get; }
+
+        private void Start()
+        {
+            StartCoroutine(AsyncSceneTransition());
+        }
+
+        public IEnumerator AsyncSceneTransition()
+        {
+            AsyncOperation aOperation = SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+            aOperation.allowSceneActivation = false;
+
+            yield return WaitForAsync(aOperation);
+
+            aOperation = SceneManager.UnloadSceneAsync(0, UnloadSceneOptions.None);
+
+            yield return WaitForAsync(aOperation);
+
+        }
+
+        private IEnumerator WaitForAsync(AsyncOperation operation)
+        {
+            while (!operation.isDone)
+            {
+                Debug.Log(operation.progress);
+
+                if (operation.progress >= 0.9f)
+                {
+                    operation.allowSceneActivation = true;
+                }
+
+                yield return null;
+            }
+        }
+    }
+}
