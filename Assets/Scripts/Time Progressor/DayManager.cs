72e600eaa580f6943d2d1fe44dff43d3f710109e
Jack-Pettigrew <jak.pettigrew@gmail.com>
2020-12-31 10:35:18 +0000

    Basic DayManager - Renames days from 1 - 6

---
+++ Assets/Scripts/Time Progressor/DayManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DayManager : MonoBehaviour
+{
+    [Tooltip("Ensure days are added in order - old to new dates")]
+    [SerializeField] private TextMeshProUGUI[] dayTexts;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        for (int i = 0; i < dayTexts.Length; i++)
+        {
+            dayTexts[i].text = (i + 1).ToString();
+        }
+    }
+}
